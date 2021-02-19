5a344e5ed9faf889ab9e1836dd29ee0743ed31b3
anita <75268681+anitakamani@users.noreply.github.com>
2021-02-20 03:20:12 +0330

    project initial files

---
+++ anki_audio.py
@@ -0,0 +1,25 @@
+# # import the main window object (mw) from aqt
+# from aqt import mw
+# # import the "show info" tool from utils.py
+# from aqt.utils import showInfo
+# # import all of the Qt GUI library
+# from aqt.qt import *
+#
+#
+# # We're going to add a menu item below. First we want to create a function to
+# # be called when the menu item is activated.
+
+# add audio
+# add this to the menu gear
+# if card type="German Vocabulary"
+# article = "der"
+# german = "Tepisch"
+# sentence = "der Tepisch ist schon"
+# thing="Noun_Article"+" "+"German"+"."+German_Example_Sentence
+# write thing:German_Audio
+
+def make_text(article, german, sentence):
+    text = article + " " + german + ". " + sentence
+    while text.find("  ") != -1:
+        text = text.replace("  ", " ")
+    return text
---
+++ google.py
@@ -0,0 +1,46 @@
+import requests
+from anki_audio import *
+
+
+def run(text, path):
+    """
+    Downloads from Google directly to an MP3.
+    """
+    try:
+        return net_download(
+            path,
+            'https://translate.google.com/translate_tts?ie=UTF-8&ttsspeed=1.0&client=tw-ob&tl=de&q=' + text
+        )
+    except:
+        pass
+
+
+def net_download(path, link):
+    """
+    Downloads a file to the given path from the specified target(s).
+    See net_stream() for information about available options.
+    """
+
+    payload = net_stream(link)
+    return payload
+    # with open(path, 'wb') as response_output:
+    #     response_output.write(payload)
+
+
+def net_stream(link):
+    DEFAULT_TIMEOUT = 15
+    method = 'GET'
+
+    response = requests.request(
+        method=method,
+        url=link,
+        timeout=DEFAULT_TIMEOUT,
+    )
+    payload = response.content
+    response.close()
+    return payload
+
+
+audio=run(make_text("das","Auto","das Auto ist sehr schon"), 'dsf')
+with open('myfile.wav', mode='w+b') as f:
+    f.write(audio)
