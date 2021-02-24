0ca9bc8dbd66cd0d3688ec94a5656bcd6346b3be
anita <75268681+anitakamani@users.noreply.github.com>
2021-02-24 04:01:15 +0330

    reading from anki decks

--- anki_audio.py
+++ anki_audio.py
@@ -1,26 +1,14 @@
-# # import the main window object (mw) from aqt
-# from aqt import mw
-# # import the "show info" tool from utils.py
-# from aqt.utils import showInfo
-# # import all of the Qt GUI library
-# from aqt.qt import *
-#
-#
-# # We're going to add a menu item below. First we want to create a function to
-# # be called when the menu item is activated.
-
-# add audio
-# add this to the menu gear
-# if card type="German Vocabulary"
-# article = "der"
-# german = "Tepisch"
-# sentence = "der Tepisch ist schon"
-# thing="Noun_Article"+" "+"German"+"."+German_Example_Sentence
-# write thing:German_Audio
-import file_job
+# import the main window object (mw) from aqt
 import google
+from aqt import mw
+# import the "show info" tool from utils.py
+from aqt.utils import showInfo
+# import all of the Qt GUI library
+from aqt.qt import *
+import file_job
 
-
+# We're going to add a menu item below. First we want to create a function to
+# be called when the menu item is activated.
 def make_text(article, german, sentence):
     text = article + " " + german + ". " + sentence
     while text.find("  ") != -1:
@@ -28,13 +16,35 @@ def make_text(article, german, sentence):
     return text
 
 
-article = "der"
-german = "Aufzug"
-sentence = "Der Aufzug steckt fest"
+def testFunction():
+    # get the number of cards in the current collection, which is stored in
+    # the main window
+    # cardCount = mw.col.cardCount()
+    # show a message box
+    deck_name = "Audio"
+    did = mw.col.decks.id(deck_name)
+    cids = mw.col.decks.cids(did)
+    i = 0
+    while i < len(cids):
+        cid = cids[i]
+        card = mw.col.getCard(cid)
+        note = card.note()
+        fields = note.values()
+        article = fields[5]
+        german = fields[0]
+        sentence = fields[2]
+
+        text = make_text(article, german, sentence)
+        if i % 2 == 0:
+            path = file_job.path_cache(text)
+            google.run(text, path)
+
+        i += 1
 
 
-text = make_text(article, german, sentence)
-path = file_job.path_cache(text)
-google.run(text, path)
-
-
+# create a new menu item, "test"
+action = QAction("test", mw)
+# set it to call testFunction when it's clicked
+action.triggered.connect(testFunction)
+# and add it to the tools menu
+mw.form.menuTools.addAction(action)
