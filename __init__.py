68e77697fd0a39b0825d707a8b16f3af134c1115
Anita Kamani <75268681+anitakamani@users.noreply.github.com>
2021-03-01 01:41:48 +0330

    Update __init__.py
    the new one

--- __init__.py
+++ __init__.py
@@ -1,15 +1,10 @@
-# import the main window object (mw) from aqt
 from . import google
 from aqt import mw
-# import the "show info" tool from utils.py
 from aqt.utils import showInfo
-# import all of the Qt GUI library
 from aqt.qt import *
 from . import file_job
 
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
 def make_text(article, german, sentence):
     text = article + " " + german + ". " + sentence
     while text.find("  ") != -1:
@@ -18,41 +13,39 @@ def make_text(article, german, sentence):
 
 
 def testFunction():
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    # cardCount = mw.col.cardCount()
-    # show a message box
-    deck_name = "Audio"
+    deck_name = "1111::1Audio"
     did = mw.col.decks.id(deck_name)
-    cids = mw.col.decks.cids(did)
+    type_id = mw.col.backend.get_notetype_id_by_name("German Vocabulary")
+    cids = mw.col.decks.cids(did, children=True)
     i = 0
-    while i < len(cids):
-        if i % 2 == 0:
-            cid = cids[i]
-            card = mw.col.getCard(cid)
-            note = card.note()
-            fields = note.values()
-            article = fields[5]
-            german = fields[0]
-            sentence = fields[2]
-            text = make_text(article, german, sentence)
-            file_name = file_job.file_name_creator(text)
-            path = file_job.path_cache(file_name)
-            google.run(text, path)
-            file_name = mw.col.media.addFile(path)
-            file_name = "[sound:" + file_name + "]"
-            fields[4] = file_name
-            note.flush()
+    for cid in cids:
+        card = mw.col.getCard(cid)
+        card_type = card.note().mid
+        if type_id == card_type:
+            try:
+                note = card.note()
+                fields = note.values()
+                article = fields[5]
+                german = fields[0]
+                sentence = fields[2]
+                text = make_text(article, german, sentence)
+                try:
+                    file_name = file_job.file_name_creator(text)
+                    path = file_job.path_cache(file_name)
+                    google.run(text, path)
+                    file_name = mw.col.media.addFile(path)
+                    file_name = "[sound:" + file_name + "]"
+                    fields[4] = file_name
+                    note.flush()
+                    i += 1
+                except:
+                    showInfo("error %d"%text)
+            except:
+                showInfo("error")
+    showInfo("OK ;) \n\n %d cards downloaded" % i)
 
-        i += 1
 
-    showInfo("OK ;)")
-
-
-# create a new menu item, "test"
 action = QAction("Audio Add-on", mw)
 file_job.empty_cache()
-# set it to call testFunction when it's clicked
 action.triggered.connect(testFunction)
-# and add it to the tools menu
 mw.form.menuTools.addAction(action)
